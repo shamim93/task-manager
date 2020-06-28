f0770ac2ea059f26fdc329b73993fe589340504a
Shamim Hossen <shamim41.cse@gmail.com>
2020-06-29 00:10:24 +0600

    Factory and Seeder create

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use App\Task;
+use Faker\Generator as Faker;
+
+$factory->define(Task::class, function (Faker $faker) {
+    return [
+        'title' =>$faker->sentence(),
+        'description' =>$faker->paragraph(),
+        'completed' =>false
+    ];
+});
